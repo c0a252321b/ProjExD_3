f45ea4ef2824dea7053b2474bda125ab68d58b99
miharas <c0a252321b@edu.teu.ac.jpgit config -lgit config --global user.email c0a252321b@edu.teu.ac.jp>
2026-06-23 15:11:00 +0900

    練習５：複数爆弾

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -7,6 +7,7 @@ import pygame as pg
 
 WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
+NUM_OF_BOMBS = 5 #爆弾の数
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -147,10 +148,13 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200)) #Birdクラスのインスタンス作成。パラメータはタプルで指定されているので
-    bomb = Bomb((255, 0, 0), 10) #Bombクラスの新スタンス作成
+    # bomb = Bomb((255, 0, 0), 10) #Bombクラスの新スタンス作成
     beam = None  # ゲーム初期化時にはビームは存在しない
     clock = pg.time.Clock()
     tmr = 0
+
+    bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS) ]#５この爆弾リスト
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: #×ボタンが押されたら
@@ -160,7 +164,8 @@ def main():
                 beam = Beam(bird) #Beamクラスのイニシャライザでbirdが引数に指定されてる            
         screen.blit(bg_img, [0, 0])
         
-        if bomb is not None:
+        # if bomb is not None: ⇐for文に変更
+        for bomb in bombs:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
@@ -168,19 +173,22 @@ def main():
                 time.sleep(1)
                 return
         
-        if bomb is not None:
+        # if bomb is not None: ⇐for文に変更
+        for i, bomb in enumerate(bombs):
             if beam is not None: #Noneでは判定ができない
                 if beam.rct.colliderect(bomb.rct): #beamがbombに当たったら
-                    bomb = None 
-                    beam = None
                     bird.change_img(6, screen) #birdクラスのchange_imgメソッドの引数の形に合わせる
                     pg.display.update()
+                    beam = None
+                    bombs[i] = None    
+        bombs = [bomb for bomb in bombs if bomb is not None] #Noneでない要素のみで新しくリストを作る
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None: #beamが出現していたら
             beam.update(screen) #Beamクラスのupdateメソッドではscreenにblitしているからしているから   
-        if bomb is not None: 
+        # if bomb is not None: ⇐for文に変更
+        for bomb in bombs:
             bomb.update(screen)
         pg.display.update()
         tmr += 1
